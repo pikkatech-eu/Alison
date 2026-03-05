24787178261dd4e8718d5625725873aa2b569b13
Stanislav <stanislav@pikkatech.eu>
2026-03-05 15:32:14 +0100

    added NLPUtils

---
+++ Code/CSharp/Alison.Library/NLPUtils/ExtendedTokenizer.cs
@@ -0,0 +1,23 @@
+﻿/***********************************************************************************
+* File:         ExtendedTokenizer.cs                                               *
+* Contents:     Class ExtendedTokenizer                                            *
+* Author:       Stanislav "Bav" Koncebovski (stanislav@pikkatech.eu)               *
+* Date:         2026-02-27 21:48                                                   *
+* Version:      1.0                                                                *
+* Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
+***********************************************************************************/
+
+using NLPUtils;
+
+namespace Factotum.NLPUtils
+{
+	public static class ExtendedTokenizer
+	{
+		public static string[] TokenizeToWords(string source)
+		{
+			var tokens = Tokenizer.Tokenize(source).Where(e=>e.Type == TokenType.Word).ToArray();
+
+			return tokens.Select(t=>t.Content).ToArray();
+		}
+	}
+}
